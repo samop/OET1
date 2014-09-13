05d3032464b163c6f464726f4216b2f9a759b320
Samo Penic <samo.penic@gmail.com>
2014-09-13 22:26:26 +0200

    Fix in scripts

---
+++ scrips/oet1-home-revert.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+HOMEDIR="/home/samo/Desktop/OET1"
+
+rm -rf $HOMEDIR
+mkdir -p $HOMEDIR
+cp /usr/local/OET1/octave/*.m $HOMEDIR
+
---
+++ scrips/oet1-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p /usr/local
+cd /usr/local
+git clone https://github.com/samop/OET1.git
+
