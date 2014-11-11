2b2f26505721d21976d981e7768cd66b158a52ba
Samo Penic <samo.penic@fe.uni-lj.si>
2014-11-11 10:46:46 +0100

    fix in install script

--- scripts/oet1-install.sh
+++ scripts/oet1-install.sh
@@ -3,4 +3,9 @@
 mkdir -p /usr/local
 cd /usr/local
 git clone https://github.com/samop/OET1.git
+#vse izvrsne datoteke v bin direktorij.
+cp /usr/local/OET1/scripts/*.sh /usr/local/bin;
+#init.d skripte v /etc
+cp /usr/local/OET1/init.d/oet1 /etc/init.d;
+chmod a+x /etc/init.d/oet1;
 
